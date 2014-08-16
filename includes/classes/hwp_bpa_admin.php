22801bff0405bc9dcf50b94391bd215b0871f028
Josh Pollock <jpollock412@gmail.com>
2014-08-16 01:32:16 -0400

    localize pods object into admin view

--- includes/classes/hwp_bpa_admin.php
+++ includes/classes/hwp_bpa_admin.php
@@ -21,7 +21,10 @@ class hwp_bpa_admin {
 	function admin_page() {
 		$view = apply_filters( 'hwp_bpa_admin_page_file', trailingslashit( HWP_BPA_PATH ).'includes/views/admin-page.php' );
 		if ( file_exists( $view ) ) {
-			return pods_view( $view, null, DAY_IN_SECONDS, 'transient', true );
+			$pods = pods( 'topic' );
+
+			return pods_view( $view, compact( array( 'pods' => $pods ) ), DAY_IN_SECONDS, 'transient', true );
+
 		}
 		else {
 			return __( sprintf( 'The admin page can not be loaded as the chosen view %1s does not exist. Please check the method %1s in the class %2s or check the "hwp_bpa_admin_page_file" filter.', __METHOD__, __CLASS__ ), 'hwp_bpa' );
