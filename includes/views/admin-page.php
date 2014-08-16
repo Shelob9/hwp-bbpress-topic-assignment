73b4c1f21b09119c5bafd75de137d28aa9d45343
Josh Pollock <jpollock412@gmail.com>
2014-08-16 01:31:58 -0400

    notes on what to do in admin view

--- includes/views/admin-page.php
+++ includes/views/admin-page.php
@@ -1,10 +1,17 @@
 <?php
 /**
- * @TODO What this does.
+ * View for the admin page
  *
- * @package   @TODO
+ * @package   @hwp_bpa
  * @author    Josh Pollock <Josh@JoshPress.net>
  * @license   GPL-2.0+
  * @link      
  * @copyright 2014 Josh Pollock
  */ 
+
+/*
+ tabs:
+ foreach assignable user( assigned not resolved, assigned resolved)
+	all unassigned
+foreach topic: assign form?
+ */
